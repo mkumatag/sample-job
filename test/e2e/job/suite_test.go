e77aca9311d2663eb0d08c718526c59eb3e22c19
Ragni Garg <ragni12@in.ibm.com>
2020-06-01 10:10:09 +0000

    adding changes

--- test/e2e/job/suite_test.go
+++ test/e2e/job/suite_test.go
@@ -1,16 +1,28 @@
-package job
-
+package job 
 import (
+	"flag"
 	"testing"
-
+	 "k8s.io/klog/v2"
 	"k8s.io/kubernetes/test/e2e/framework"
-
+	frameworkconfig "k8s.io/kubernetes/test/e2e/framework/config"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
-
+// handleFlags sets up all flags and parses the command line.
+func handleFlags() {
+        frameworkconfig.CopyFlags(frameworkconfig.Flags, flag.CommandLine)
+        framework.RegisterCommonFlags(flag.CommandLine)
+        framework.RegisterClusterFlags(flag.CommandLine)
+        flag.Parse()
+        framework.AfterReadingAllFlags(&framework.TestContext)
+}
 func init() {
-	framework.TestContext.KubeConfig = "/Users/manjunath/.kube/config"
+	testing.Init()
+	handleFlags()
+	if framework.TestContext.KubeConfig == ""{
+                klog.Infof("Couldnt get kubeconfig. set environment variable KUBECONFIG,Path to kubeconfig containing embedded authinfo")
+                klog.Infof("Will try to use in-cluster config if KUBECONFIG is not available")
+	}
 }
 func TestCart(t *testing.T) {
 	RegisterFailHandler(Fail)
