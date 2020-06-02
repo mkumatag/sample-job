5cdf44d2954ef1738c7093633d83f1efc78f5388
Ragni Garg <ragni12@in.ibm.com>
2020-06-02 04:39:40 +0000

    adding changes

--- test/e2e/job/suite_test.go
+++ test/e2e/job/suite_test.go
@@ -1,25 +1,27 @@
-package job 
+package job
+
 import (
 	"flag"
-	"testing"
-	 "k8s.io/klog/v2"
-	"k8s.io/kubernetes/test/e2e/framework"
-	frameworkconfig "k8s.io/kubernetes/test/e2e/framework/config"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"k8s.io/klog/v2"
+	"k8s.io/kubernetes/test/e2e/framework"
+	frameworkconfig "k8s.io/kubernetes/test/e2e/framework/config"
+	"testing"
 )
+
 // handleFlags sets up all flags and parses the command line.
 func handleFlags() {
-        frameworkconfig.CopyFlags(frameworkconfig.Flags, flag.CommandLine)
-        framework.RegisterCommonFlags(flag.CommandLine)
-        framework.RegisterClusterFlags(flag.CommandLine)
-        flag.Parse()
-	 if framework.TestContext.KubeConfig == ""{
-                klog.Infof("Couldnt get kubeconfig. set environment variable KUBECONFIG,Path to kubeconfig containing embedded authinfo")
-                klog.Infof("Will try to use in-cluster config if KUBECONFIG is not available")
-        }
+	frameworkconfig.CopyFlags(frameworkconfig.Flags, flag.CommandLine)
+	framework.RegisterCommonFlags(flag.CommandLine)
+	framework.RegisterClusterFlags(flag.CommandLine)
+	flag.Parse()
+	if framework.TestContext.KubeConfig == "" {
+		klog.Infof("Couldnt get kubeconfig. set environment variable KUBECONFIG,Path to kubeconfig containing embedded authinfo")
+		klog.Infof("Will try to use in-cluster config if KUBECONFIG is not available")
+	}
 
-        framework.AfterReadingAllFlags(&framework.TestContext)
+	framework.AfterReadingAllFlags(&framework.TestContext)
 }
 func init() {
 	testing.Init()
